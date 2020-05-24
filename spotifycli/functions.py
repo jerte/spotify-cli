262b6248da2ac6bb3804dfcef2b6ffefea779560
Josh <joshrthomas@protonmail.com>
2020-05-24 11:49:02 -0400

    working

--- spotifycli/functions.py
+++ spotifycli/functions.py
@@ -5,31 +5,41 @@ import spotipy.util as util
 
 # pause playback
 def pause(sp, arg_list):
-    if arg_list:
+    if len(arg_list) > 1:
         return "bad usage!"
     sp.pause_playback()
     return None
 
 # skip to next track
 def next_trak(sp, arg_list):
-    if arg_list:
+    if len(arg_list) > 1:
         return "bad usage!"
     sp.next_track()
     return None
 
 # skip to previous track
 def previous(sp, arg_list):
-    if arg_list:
+    if len(arg_list) > 1:
         return "bad usage!"
     sp.previous_track()
     return None
 
 # get track currently playing
 def current(sp, arg_list):
-    if arg_list:
+    if len(arg_list) > 1:
         return "bad usage!"
     else:
         return sp.currently_playing()
+
+# reshuffles current playback
+def shuffle(sp, arg_list):
+    if len(arg_list) > 1:
+        return "bad usage!"
+    else:
+        sp.shuffle(state=False)
+        sp.shuffle(state=True)
+        sp.next_track()
+    return None
 
 ''' start or resume a user's playback
     options
@@ -41,8 +51,14 @@ def current(sp, arg_list):
         album [name] (might ask to confirm artist)
 '''
 def play(sp, arg_list):
-    if arg_list:
+    if len(arg_list) > 1:
         print("in progress!")
+
+        play_options = { 'shuffle':,
+                         'track':,
+                         'playlist':,
+                         'radio':,
+                         'album':}
     else:
         sp.start_playback()
     return None
@@ -71,6 +87,10 @@ def list(sp, arg_list):
         
 '''
 def add(sp, arg_list):
+    if len(arg_list) > 1:
+        print('placeholder')
+    else:
+        print('Usage:\nspotifycli add [this / song name / playlist name / album name] to [playlist name / queue]')
     return None
 
 def help_(sp, arg_list):
--- spotifycli/spotifycli.py
+++ spotifycli/spotifycli.py
@@ -1,64 +1,52 @@
 import sys
+import os
+from functions import *
 import spotipy
 import spotipy.util as util
-from functions import *
 
+# command line options
 func_dict = {'pause':pause, 'next':next_trak, 'previous':previous,
         'play':play, 'current':current}
-
+client_id = '30ef960eff5741d0abb4cc9a8590a90f'
+client_secret='2924697078f2489caa831756d878fa54'
+redirect_uri = "http://localhost:7777/callback/"
 scope = 'user-read-currently-playing user-modify-playback-state'
 
-write_username = False
-
-try:
-    # username is only data kept right now, this will change
-    with open('app_data.txt', 'r+') as f:
-        username = f.readline()
-        if username[0:5]=="user=":
-            username = username[5:-1]
-
-except FileNotFoundError:
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-        write_username = True
-    else:
-        print('no username!')
-        sys.exit(0)
-
-token = util.prompt_for_user_token(username, scope,
-        client_id='30ef960eff5741d0abb4cc9a8590a90f',
-        client_secret='2924697078f2489caa831756d878fa54',
-        redirect_uri='http://localhost:777/callback/')
-    
-if token:
-    sp = spotipy.Spotify(auth=token)
-    #sp.trace = False
-    if write_username:
-        with open('app_data.txt', 'w') as f:
-            f.write("user="+username)
-
-    if len(sys.argv) > 1:
-        
-        if not write_username:
-            if len(sys.argv) > 2:
-                result = func_dict[sys.argv[1]](sp, sys.argv[2:]) 
+# function to run cli
+def run_cli(username, write_username):
+        token = util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri)
+        if token:
+            if write_username:
+                with open('app_data.txt', 'w') as f:
+                    f.write("user="+username)
             else:
-                result = func_dict[sys.argv[1]](sp, '')
+                if len(sys.argv) > 1:
+                    sp = spotipy.Spotify(auth=token)
+                    try:
+                        result = fuct_dict[sys.argv[1]](sp, sys.argv[1:])
+                        if result:
+                            print(result)
+                    except KeyError:
+                        print('function \''+ sys.argv[1] + '\' not found')
+                        sys.exit(0)
+                else:
+                    print('no function provided')
+
+# get username, from file or passed cli option, then run cli
+if __name__ == "__main__":
+    try:
+        with open('app_data.txt', 'r+') as f:
+            username = f.readline()
+            if username[0:5]=="user=":
+                username = username[5:-1]
+
+            run_cli(username, write_username=False)
+
+    except FileNotFoundError:
+        if len(sys.argv) > 1:
+            username = sys.argv[1]
+
+            run_cli(username, write_username=True)
         else:
-            if len(sys.argv) > 3:
-                result = func_dict[sys.argv[2]](sp, sys.argv[3:])
-            elif len(sys.argv) > 2:
-                result = func_dict[sys.argv[2]](sp, '')
-            else:
-                result = None
-        
-        if result:
-            print(result)
-        
-    else:
-        print('no function!')
-    
-else:
-    print(f"Can't get token for {username}")
-
-
+            print('Sign in by passing Spotify username as option')
+            sys.exit(0)
